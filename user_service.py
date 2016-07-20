f8ecdb08379938218e2226dc79ccbfdb0f756bb3
ansaev <ansaev@gmail.com>
2016-07-20 18:59:19 +0300

    get ussers post user

--- user_service.py
+++ user_service.py
@@ -1,8 +1,9 @@
 import logging
 from json import loads, dumps
 from uuid import uuid4
+from math import ceil
 import tornadoredis
-from config import redis_users_settings
+from config import redis_users_settings, LIST_USERS_KEY, TRANSACTION_ATTEMPTS
 from tornado.gen import coroutine, Task
 from tornado.web import Application, RequestHandler
 
@@ -32,6 +33,8 @@ class UserInfoHandler(BaseUserHandler):
             self.finish()
             return
         result = {}
+        self.finish()
+        return
 
     @coroutine
     def post(self, user_id):
@@ -48,13 +51,42 @@ class UsersInfoHandler(BaseUserHandler):
     @coroutine
     def get(self):
         # get list of users
-        users = yield Task(self.redis_users.keys, pattern='*')
+        try:
+            page = int(self.get_argument('page'))
+        except ValueError:
+            self.set_status(status_code=400, reason='page should be int')
+            self.finish()
+            return
+        try:
+            limit = int(self.get_argument('limit'))
+        except ValueError:
+            self.set_status(status_code=400, reason='limit should be int')
+            self.finish()
+            return
+        if limit < 1:
+            self.set_status(status_code=400, reason='limit should be > 0')
+            self.finish()
+            return
+        if page < 1:
+            self.set_status(status_code=400, reason='page should be > 0')
+            self.finish()
+            return
+        users_num = yield Task(self.redis_users.llen, LIST_USERS_KEY)
+        pages = ceil(float(users_num)/float(limit))
+        if page > pages:
+            self.set_status(status_code=400, reason='page should be <= %d' % pages)
+            self.finish()
+            return
+        start_index = page * limit
+        end_index = start_index + limit
+        users = yield Task(self.redis_users.lrange, key=LIST_USERS_KEY, start=start_index, end=end_index)
         self.write(dumps(users))
         self.finish()
+        return
 
     @coroutine
     def post(self):
-        # create user and return id
+        #create user and return id
         try:
             name = str(self.get_body_argument('name'))
         except BaseException as encode_name_exep:
@@ -64,7 +96,24 @@ class UsersInfoHandler(BaseUserHandler):
             return
         try:
             user_id = str(uuid4())
-            resp = yield Task(self.redis_users.set, key=user_id, value=name)
+            pipe = self.redis_users.pipeline(transactional=True)
+            pipe.set(key=user_id, value=name)
+            pipe.lpush(LIST_USERS_KEY, user_id)
+            errors = []
+            for i in range(TRANSACTION_ATTEMPTS):
+                try:
+                    resp_s, resp_p = yield Task(pipe.execute)
+                    break
+                except BaseException as e:
+                    errors.append(e)
+                    continue
+            else:
+                self.set_status(status_code=500, reason='internal error')
+                self.finish()
+                msg = [str(e) for e in errors]
+                msg = ';'.join(msg)
+                logging.critical(msg='add new user transaction execution: ' + msg)
+                return
             self.write(dumps({'id': user_id, 'name': name}))
         except BaseException as ecnode_uuid_exep:
             self.set_status(status_code=500, reason='internal error')
