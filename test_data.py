a714b4a250b570b5d3617b0b0d8bf95d1ab8afba
ansaev <ansaev@gmail.com>
2016-07-21 16:47:59 +0300

    readme and test change

--- test_data.py
+++ test_data.py
@@ -3,9 +3,10 @@ import uuid
 import redis
 from config import redis_users_settings, LIST_USERS_KEY
 
+from pip._vendor import requests
+
 client = redis.Redis(host='127.0.0.1', port=6379, db=24)
 for i in range(1000000):
     user_id = str(uuid.uuid4())
-    client.set(name=user_id, value=user_id)
-    client.lpush(LIST_USERS_KEY, user_id)
+    requests.post(url='http://localhost:6665/users', data={'name': user_id[:6]})
 
